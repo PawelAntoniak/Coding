b6cb3742be58ae47f9c68fb24458442c26bc8b40
Pawel Antoniak <pantoniak@gmail.com>
2020-11-16 17:58:48 +0100

    first commit

---
+++ utils.js
@@ -0,0 +1,27 @@
+
+// var button = document.getElementById('btn');
+// var div = document.getElementById('aaa');
+
+// button.addEventListener('click', function(){
+    
+//     if(div.style.display='hidden'){
+//         div.style.display='block'
+//     } else {
+//         div.style.display = 'hidden'
+//     }
+   
+// })
+
+// dlaczego powyższe niedziała w pluralsight podobne rozwiazanie tak?
+
+//let i = 0
+var button = document.getElementById('btn').addEventListener('click', function(){
+    for(let i = 0; i<10; i++){
+        if(i > 5){
+            
+        }
+    }
+    
+})
+
+
